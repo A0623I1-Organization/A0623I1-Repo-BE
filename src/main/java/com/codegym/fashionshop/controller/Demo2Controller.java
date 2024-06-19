c1daad1a5d89faafa265efa76ab52ac469de0ab2
vankhang49 <dvkhang49@gmail.com>
2024-06-19 20:17:53 +0700

    up

---
+++ src/main/java/com/codegym/fashionshop/controller/Demo2Controller.java
@@ -0,0 +1,16 @@
+package com.codegym.fashionshop.controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth/demo-controller")
+@CrossOrigin("*")
+public class Demo2Controller {
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello from security");
+    }
+}
