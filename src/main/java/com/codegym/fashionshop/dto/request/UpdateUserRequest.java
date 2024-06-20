c67ed9b1da5c088440ef31afd0c4ffba351fd933
levanquy0101 <quyle.01012002@gmail.com>
2024-06-20 12:03:55 +0700

    up

--- src/main/java/com/codegym/fashionshop/dto/request/UpdateUserRequest.java
+++ src/main/java/com/codegym/fashionshop/dto/request/UpdateUserRequest.java
@@ -1,43 +1,33 @@
-package com.codegym.fashionshop.dto.respone;
+package com.codegym.fashionshop.dto.request;
 
 import com.codegym.fashionshop.entities.AppRole;
-import com.codegym.fashionshop.entities.AppUser;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthenticationResponse {
-    private int statusCode;
-
-    private String error;
-
-    private String message;
-
-    private String token;
-
+public class UpdateUserRequest {
     private Long userId;
 
     private String username;
 
-    private LocalDate dateCreate;
+    private String password;
+
+    private String userCode;
 
     private String avatar;
 
     private String fullName;
 
-    private String userCode;
+    private LocalDate dateOfBirth;
 
     private Integer gender;
-
-    private LocalDate dateOfBirth;
 
     private String phoneNumber;
 
@@ -47,7 +37,5 @@ public class AuthenticationResponse {
 
     private AppRole role;
 
-    private List<AppUser> users;
-
-    private List<AppRole> roles;
+    private LocalDate dateCreate;
 }
--- src/main/java/com/codegym/fashionshop/service/impl/AuthenticationService.java
+++ src/main/java/com/codegym/fashionshop/service/impl/AuthenticationService.java
@@ -3,10 +3,13 @@ package com.codegym.fashionshop.service.impl;
 import com.codegym.fashionshop.dto.UserInforUserDetails;
 import com.codegym.fashionshop.dto.request.AuthenticationRequest;
 import com.codegym.fashionshop.dto.request.RegisterRequest;
+import com.codegym.fashionshop.dto.request.UpdateUserRequest;
 import com.codegym.fashionshop.dto.respone.AuthenticationResponse;
 import com.codegym.fashionshop.dto.respone.UserInformation;
+import com.codegym.fashionshop.entities.AppRole;
 import com.codegym.fashionshop.entities.AppUser;
 import com.codegym.fashionshop.exceptions.UserIsExistException;
+import com.codegym.fashionshop.repository.IRoleRepository;
 import com.codegym.fashionshop.repository.IUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +19,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
     @Autowired
     private IUserRepository userRepository;
+
+    @Autowired
+    private IRoleRepository roleRepository;
 
     private final PasswordEncoder passwordEncoder;
 
@@ -53,10 +61,9 @@ public class AuthenticationService {
                 .enabled(true)
                 .build();
         userRepository.save(user);
-        UserInforUserDetails userDetails = new UserInforUserDetails(user);
-        var jwtToken = jwtService.generateToken(userDetails);
         return AuthenticationResponse.builder()
-                .token(jwtToken)
+                .statusCode(200)
+                .message("User registered successfully")
                 .build();
     }
 
@@ -84,12 +91,14 @@ public class AuthenticationService {
     }
 
 
-    public AuthenticationResponse getMyInfo(String username){
+    public AuthenticationResponse getMyInfo(String username) {
         try {
             AppUser user = userRepository.findByUsername(username);
+            List<AppRole> roles = roleRepository.findAll();
             return AuthenticationResponse.builder()
                     .statusCode(200)
                     .message("Successfully!")
+                    .userId(user.getUserId())
                     .username(user.getUsername())
                     .userCode(user.getUserCode())
                     .dateCreate(user.getDateCreate())
@@ -101,11 +110,65 @@ public class AuthenticationService {
                     .gender(user.getGender())
                     .avatar(user.getAvatar())
                     .address(user.getAddress())
+                    .roles(roles)
                     .build();
-        }catch (Exception e){
+        } catch (Exception e) {
             return AuthenticationResponse.builder()
                     .statusCode(500)
                     .message("Error occurred while getting user info: " + e.getMessage()).build();
+        }
+    }
+
+    public AuthenticationResponse updateUser(Long userId, UpdateUserRequest updatedUser) {
+        try {
+            Optional<AppUser> user = userRepository.findById(userId);
+            List<AppRole> roles = roleRepository.findAll();
+            if (user.isPresent()) {
+                AppUser appUser = user.get();
+                appUser.setAvatar(updatedUser.getAvatar());
+                appUser.setFullName(updatedUser.getFullName());
+                appUser.setGender(updatedUser.getGender());
+                appUser.setEmail(updatedUser.getEmail());
+                appUser.setPhoneNumber(updatedUser.getPhoneNumber());
+                appUser.setRole(updatedUser.getRole());
+                appUser.setDateCreate(updatedUser.getDateCreate());
+                appUser.setDateOfBirth(updatedUser.getDateOfBirth());
+
+                // Check if password is present in the request
+                if (updatedUser.getPassword() != null && !updatedUser.getPassword().isEmpty()) {
+                    // Encode the password and update it
+                    appUser.setEncryptedPassword(passwordEncoder.encode(updatedUser.getPassword()));
+                }
+
+                AppUser appUser1 = userRepository.save(appUser);
+                return AuthenticationResponse.builder()
+                        .statusCode(200)
+                        .message("User updated successfully")
+                        .userId(appUser1.getUserId())
+                        .username(appUser1.getUsername())
+                        .userCode(appUser1.getUserCode())
+                        .dateCreate(appUser1.getDateCreate())
+                        .dateOfBirth(appUser1.getDateOfBirth())
+                        .email(appUser1.getEmail())
+                        .phoneNumber(appUser1.getPhoneNumber())
+                        .role(appUser1.getRole())
+                        .fullName(appUser1.getFullName())
+                        .gender(appUser1.getGender())
+                        .avatar(appUser1.getAvatar())
+                        .address(appUser1.getAddress())
+                        .roles(roles)
+                        .build();
+            } else {
+                return AuthenticationResponse.builder()
+                        .statusCode(404)
+                        .message("User not found for update")
+                        .build();
+            }
+        } catch (Exception e) {
+            return AuthenticationResponse.builder()
+                    .statusCode(500)
+                    .message("Error occurred while updating user: " + e.getMessage())
+                    .build();
         }
     }
 }
