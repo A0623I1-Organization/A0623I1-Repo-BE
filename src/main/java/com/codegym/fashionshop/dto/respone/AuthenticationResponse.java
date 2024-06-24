9baf0cca1c203dbf7634ec2471a3f8b211faf9e3
vankhang49 <dvkhang49@gmail.com>
2024-06-24 18:06:44 +0700

    KhangDV add new entities and user fuctions

--- src/main/java/com/codegym/fashionshop/dto/respone/AuthenticationResponse.java
+++ src/main/java/com/codegym/fashionshop/dto/respone/AuthenticationResponse.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -47,7 +48,7 @@ public class AuthenticationResponse {
 
     private AppRole role;
 
-    private List<AppUser> users;
+    private Page<AppUser> users;
 
     private List<AppRole> roles;
 }
