f56fdfe8c0c311c38246fc7a46b3381b2c36b5f0
vankhang49 <dvkhang49@gmail.com>
2024-07-06 14:40:28 +0700

    KhangDV

--- src/main/java/com/codegym/fashionshop/config/SecurityConfiguration.java
+++ src/main/java/com/codegym/fashionshop/config/SecurityConfiguration.java
@@ -28,7 +28,7 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable())  // Sử dụng phương pháp mới để vô hiệu hóa CSRF
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/**","api/public/**").permitAll()
-                        .requestMatchers("/api/auth/**").permitAll() // Tạm thời không xác thực, sau đổi sang .authenticated()
+                        .requestMatchers("/api/auth/**", "/api/users/roles").permitAll() // Tạm thời không xác thực, sau đổi sang .authenticated()
                         .requestMatchers("/api/users/**", "/api/dashboard/**", "/auth/get-profile", "/auth/update-password/{userId}").authenticated()
                 )
                 .sessionManagement(session -> session
